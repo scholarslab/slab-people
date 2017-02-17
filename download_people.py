3533de534e37a678b25c326604df8e65fa281478
Eric Rochester <erochest@virginia.edu>
2017-02-17 07:06:51 -0500

    Use Python instead of shell/make.

---
+++ download_people.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+
+import csv
+import requests
+from urllib.parse import urljoin
+from collections import namedtuple
+
+
+HOST = 'http://scholarslab.org/'
+OUTPUT = 'people.csv'
+FIELDS = ('date', 'name', 'category', 'description')
+
+Person = namedtuple('Person', FIELDS)
+
+
+def get_people(host=HOST):
+    """Download all posts with post-type of people."""
+    resp = requests.get(
+        urljoin(host, '/wp-json/posts'),
+        params={'type': 'people'},
+        )
+    return resp.json()
+
+
+def to_person(p):
+    """Extracts the information we want out of a WP people post-type."""
+    try:
+        return Person(
+            p['date'],
+            p['title'],
+            ', '.join(cat['name'] for cat in p['terms']['people-category']),
+            p['content'],
+            )
+    except:
+        import pprint
+        print('ERROR ON: {}'.format(pprint.pformat(p)))
+        raise
+
+
+def main():
+    with open(OUTPUT, 'w') as fout:
+        writer = csv.writer(fout)
+        writer.writerow(Person._fields)
+        writer.writerows(to_person(p) for p in get_people())
+
+
+if __name__ == '__main__':
+    main()
